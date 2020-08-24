d73a19c25e1ffb18cd3dffc2c531c741c829e1d9
mariana.vns <mariana.vns@gmail.com>
2020-08-24 15:30:35 -0300

    Apaguei as respostas da aula da minha branch

--- exercicios-for.js
+++ exercicios-for.js
@@ -2,46 +2,17 @@ const input = require('readline-sync')
 
 // 1) crie uma função que receba uma string e devolva seu valor em letras maiúsculas, separadas por um espaço. Exemplo: 'banana' -> 'B A N A N A'
 
-
-
 // 2) crie uma função que receba um número e devolva uma soma progressiva. Por exemplo, recebendo o número 5, a função deve retornar 15, ou seja, o resultado de 1 + 2 + 3 + 4 + 5. 
 
-function soma(num) {
-  let total = 0 
-  for (let i = 0; i <= num; i++) {
-    // total = total + i
-    total += i
-  }
-  return total
-}
 
 // 3) crie um programa que pergunta seu nome. Depois que digitar o nome, o programa deverá responder 'Olá [nome]'.
 // Enquanto digitar qualquer palavra, print o que foi digitado mas avise que, para sair, é só dizer 'Tchau'.
 // O programa vai dizer 'Tchau [nome]' e finalizar.
 
-function imprimeComandos() {
-
-  let nome = input.question('digite seu nome: ')
-  console.log(`Olá, ${nome}`)
-
-  let texto = ''
-  
-  do {
-    texto = input.question('digite algo ou "Tchau" para sair: ')
-    console.log(`Você digitou: ${texto}`)
-  } while (texto != 'Tchau')
-
-  console.log(`Tchau, ${nome}!`)
-}
 
 // 4) crie uma função que receba dois números e exiba no console uma contagem regressiva entre eles, contando de dois em dois números. Por exemplo: recebendo o número inicial 20 e o final 0, a função deverá imprimir em sequência 20 18 16 14 12 10 8 6 4 2 0.
 
                             // 20        0
-function contagemRegressiva(numInicial, numFinal) {
-  for (let i = numInicial; i >= numFinal; i -= 2) {
-    console.log(i)
-  }
-}
 
 // 5) crie uma função que receba um número e imprima no console de 0 até o número informado. Além disso, cada vez que imprimir o número, informe se é par ou ímpar. Exemplo: recebendo 5, deve imprimir: "0 é par", "1 é ímpar", "2 é par", "3 é ímpar", "4 é par", "5 é ímpar". 
 
--- exercicios-funcoes.js
+++ exercicios-funcoes.js
@@ -2,60 +2,7 @@
 
 // 1) crie uma função para cada uma das operações básicas (soma, subtração, multiplicação e divisão), cada uma delas recebendo 2 números como parâmetros e retornando o resultado
 
-function soma(numero1, numero2) {
-  return numero1 + numero2
-}
-
-function subtracao(numero1, numero2) {
-  return numero1 - numero2
-}
-
-function multiplicacao(numero1, numero2) {
-  return numero1 * numero2
-}
-
-function divisao(numero1, numero2) {
-  return numero1 / numero2
-}
-
-function operacoes(num1, num2, operacao) {
-  switch (operacao) {
-    case "soma":
-      return num1 + num2
-    case "subtracao":
-      return num1 - num2
-    case "multiplicacao":
-      return num1 * num2
-    case "divisao":
-      return num1 / num2
-    }
-}
-
-console.log(soma(2, 2))
-console.log(subtracao(4, 2))
-console.log(multiplicacao(4, 4))
-console.log(divisao(4, 2))
-console.log("CONSOLE LOG DA FUNCAO OPERAÇÕES", operacoes(2, 2, "subtracao"))
-
 // 2) para esse exercício, as funções de multiplicação e divisão não devem aceitar valores iguais a 0 (zero). Adicione esta lógica nas funções.
-
-function multiplicaSemZero(num1, num2) {
-  if (num1 === 0 || num2 === 0) {
-    return `não queremos zero`
-  }
-  return num1 * num2
-}
-
-console.log(multiplicaSemZero(4, 3))
-
-function divideSemZero(num1, num2) {
-  if (num1 === 0 || num2 === 0) {
-    return `não queremos zero`
-  }
-  return num1 / num2
-}
-
-console.log(divideSemZero(10, 2))
 
 // 3) crie uma função que chame internamente qualquer uma das funções acima e imprima o resultado no formato de String. Exemplo: "O resultado da operação é X" (sendo X o valor do resultado).
 
