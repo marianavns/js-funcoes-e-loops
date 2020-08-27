09e4940a22f33e980cd8391c5a93ac91d13eeabf
mariana.vns <mariana.vns@gmail.com>
2020-08-27 17:08:52 -0300

    Fiz o exercicio 2

--- meus-exercicios-funcoes.js
+++ meus-exercicios-funcoes.js
@@ -7,51 +7,104 @@ Você pode dar os nomes que quiser para as funções e variáveis, apenas lembre
 /*####### SOMA #######*/
 
     const input = require('readline-sync')
+
     console.log("#### Somando dois numeros ####")
+
     let valor1 = parseInt(input.question ("Digite o primeiro numero: "))
     let valor2 = parseInt(input.question ("Digite o segundo numero: "))
+
         function soma (num1, num2){
             return num1 + num2
         }
-    console.log("O valor da soma dos números " + valor1 + " e " + valor2 + " é igual a " + soma(valor1,valor2))  */
+    console.log("O valor da soma dos números " + valor1 + " e " + valor2 + " é igual a " + soma(valor1,valor2))
 
 
 /*####### SUBTRAÇÃO #######*/
 
     const input = require('readline-sync')
+
     console.log("#### Subtraindo dois numeros ####")
+
     let valor1 = parseInt(input.question ("Digite o primeiro numero: "))
     let valor2 = parseInt(input.question ("Digite o segundo numero: "))
+
         function subtrai (num1, num2){
             return num1 - num2
         }
-    console.log("A diferença entre o primeiro número (" + valor1 + ") e o segundo (" + valor2 + ") é igual a " + subtrai(valor1,valor2)) */
+    console.log("A diferença entre o primeiro número (" + valor1 + ") e o segundo (" + valor2 + ") é igual a " + subtrai(valor1,valor2))
 
 /*####### MULTIPLICACAO #######*/
     
     const input = require('readline-sync')
+
     console.log("#### Multiplicando dois numeros ####")
+
     let valor1 = input.question("Digite o primeiro numero: ")
     let valor2 = input.question("Digite o segundo numero: ")
+
         const multiplica = function (num1, num2){
             return num1 * num2
         }
-    console.log("A multiplicacao dos numeros " + valor1 + " e " + valor2 + " é igual a " + multiplica(valor1, valor2)) */
+    console.log("A multiplicacao dos numeros " + valor1 + " e " + valor2 + " é igual a " + multiplica(valor1, valor2))
 
 /*####### DIVISAO #######*/
 
     const input = require('readline-sync')
+
     console.log("#### Dividindo dois numeros ####")
+
     let valor1 = parseInt(input.question ("Digite o primeiro numero: "))
     let valor2 = parseInt(input.question ("Digite o segundo numero: "))
+
         function divide (num1, num2){
             return num1 / num2
         }
     console.log("A divisao do primeiro número (" + valor1 + ") pelo segundo (" + valor2 + ") é igual a " + divide(valor1,valor2)) 
 
- /* 2) para esse exercício, as funções de multiplicação e divisão não devem aceitar valores iguais a 0 (zero). Adicione esta lógica nas funções.
+ /* 2) para esse exercício, as funções de multiplicação e divisão não devem aceitar valores iguais a 0 (zero). Adicione esta lógica nas funções. */
 
- 3) crie uma função que chame internamente qualquer uma das funções acima e imprima o resultado no formato de String. Exemplo: "O resultado da operação é X" (sendo X o valor do resultado).
+
+ /*####### MULTIPLICAÇAO COM CONDICAO #######*/
+
+console.log("==== Multiplicando dois numeros ==== Valores iguais a zero nao sao permitidos ====")
+
+const input = require('readline-sync')
+let valor1 = input.question("Digite o primeiro numero: ")
+let valor2 = input.question("Digite o segundo numero: ")
+
+    const multiplica = function (num1, num2){
+        if (num1 == 0 || num2 == 0){
+            console.log("[X] Valores iguais a zero nao sao aceitos.")
+            } else {
+        let resultado = num1 * num2
+        console.log("A multiplicacao dos numeros " + valor1 + " e " + valor2 + " é igual a " + resultado)
+                }
+    }
+
+multiplica(valor1,valor2)
+
+
+/* ####### DIVISÃO COM CONDIÇÃO #######*/ 
+
+console.log("==== Dividindo dois numeros ==== Valores iguais a zero nao sao permitidos ====")
+
+const input = require('readline-sync')
+let valor1 = parseInt(input.question ("Digite o primeiro numero: "))
+let valor2 = parseInt(input.question ("Digite o segundo numero: "))
+
+    function divide (num1, num2){
+        if (num1 == 0 || num2 == 0){
+            console.log("[X] Valores iguais a zero nao sao aceitos")
+            } else {
+                let resultado = num1 / num2
+                console.log("A divisao do primeiro número (" + valor1 + ") pelo segundo (" + valor2 + ") é igual a " + resultado) 
+        }
+    }
+    
+divide(valor1, valor2)
+
+
+ /* 3) crie uma função que chame internamente qualquer uma das funções acima e imprima o resultado no formato de String. Exemplo: "O resultado da operação é X" (sendo X o valor do resultado).
 
  4) utilizando as funções de soma e multiplicação já feitas nos exercícios anteriores, crie uma nova função que chame essas funções e resolva a conta 36325 * (9824 + 777).
 
